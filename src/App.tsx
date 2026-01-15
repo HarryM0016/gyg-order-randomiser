f7ab7bcce800961d7df218133fe2c3b28a2fd3c4
Harry Miller <hmil0016@student.monash.edu>
2026-01-15 18:37:36 +1100

    Prettier changes

--- src/App.tsx
+++ src/App.tsx
@@ -12,22 +12,31 @@ function App() {
 
   return (
     <>
-      <a href="https://www.guzmanygomez.com.au/" target="_blank" title="Guzman Y Gomez">
+      <a
+        href="https://www.guzmanygomez.com.au/"
+        target="_blank"
+        title="Guzman Y Gomez"
+      >
         <img src={gygLogo} className="logo" />
       </a>
       <header>x</header>
       <div className="container">
         <h1>Select Criteria</h1>
         <div className="checkbox-container">
-          <VegetarianCheckbox isChecked={isVegetarian} onCheck={setIsVegetarian} />
+          <VegetarianCheckbox
+            isChecked={isVegetarian}
+            onCheck={setIsVegetarian}
+          />
           <BreakfastCheckbox isChecked={isBreakfast} onCheck={setIsBreakfast} />
         </div>
-        <button className="randomise-button" onClick={() => setOrder(randomiseOrder(isVegetarian, isBreakfast))}>
+        <button
+          className="randomise-button"
+          onClick={() => setOrder(randomiseOrder(isVegetarian, isBreakfast))}
+        >
           Randomise
         </button>
       </div>
       <p>{order}</p>
-
     </>
   );
 }
